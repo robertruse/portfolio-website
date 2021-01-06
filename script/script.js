e223d3fdaf8481dadba13ac0e65d409b270dae73
Robert Ruse <robert.a.ruse@gmail.com>
2021-01-06 04:57:43 +0200

    Add toggle button

--- script/script.js
+++ script/script.js
@@ -1,31 +1,74 @@
 /* Nav menu */
 const menuElement = document.querySelector(".nav-menu");
-const menuItemElements = document.querySelectorAll(".nav-menu a");
-const menuButton = document.querySelector(".menu-btn");
+const menuLinks = document.querySelectorAll(".nav-menu a");
+const menuListItems = document.querySelectorAll(".nav-menu li");
+const toggleButton = document.querySelector(".toggle-btn");
 
-menuButton.addEventListener("click", (e) => {
+//NodeList functions
+function addShowClass(nodeList) {
+  nodeList.forEach((item) => {
+    item.classList.add("show");
+  });
+}
+
+function removeShowClass(nodeList) {
+  nodeList.forEach((item) => {
+    item.classList.remove("show");
+  });
+}
+
+function toggleShowClass(nodeList) {
+  nodeList.forEach((item) => {
+    item.classList.toggle("show");
+  });
+}
+
+/*
+function setTransitionDelay(nodeList) {
+  nodeList.forEach((item, index) => {
+    item.style.transitionDelay = `${index / 20}s`;
+  });
+}
+
+// Add staggered 50ms transition delay ;
+setTransitionDelay(menuListItems);
+*/
+
+toggleButton.addEventListener("click", (e) => {
+  e.currentTarget.classList.toggle("show");
   menuElement.classList.toggle("show");
+  toggleShowClass(menuListItems);
 });
 
-menuItemElements.forEach((item) => {
+// Close nav menu after link click
+menuLinks.forEach((item) => {
   item.addEventListener("click", (e) => {
+    toggleButton.classList.remove("show");
     menuElement.classList.remove("show");
+    removeShowClass(menuListItems);
   });
 });
 
 // Close nav menu if clicked outside
 document.addEventListener("click", (e) => {
-  const isNav = e.target.closest("#header");
-  if (isNav) return;
+  const isNavParentElement = e.target.closest("#header");
+  if (isNavParentElement) return;
+  toggleButton.classList.remove("show");
   menuElement.classList.remove("show");
+  removeShowClass(menuListItems);
 });
 
 /* Lightbox */
 const lightboxElement = document.createElement("div");
+const imageElements = document.querySelectorAll(".work .container-images img");
+
 lightboxElement.id = "lightbox";
 document.body.appendChild(lightboxElement);
 
-const imageElements = document.querySelectorAll(".work .container-images img");
+function setImagePath(imagePath) {
+  if (screen.width > 540) return imagePath;
+  return `${imagePath.slice(0, -6)}sm.png`;
+}
 
 imageElements.forEach((item) => {
   item.addEventListener("click", (e) => {
@@ -40,12 +83,34 @@ imageElements.forEach((item) => {
   });
 });
 
+lightboxElement.addEventListener("click", (e) => {
+  if (e.target.id !== e.currentTarget.id && screen.width > 540) return;
+  lightboxElement.classList.remove("active");
+});
+
+/*
+const lightboxElement = document.createElement("div");
+lightboxElement.id = "lightbox";
+document.body.appendChild(lightboxElement);
+
+const img = document.createElement("img");
+lightboxElement.appendChild(img);
+
 function setImagePath(imagePath) {
   if (screen.width > 540) return imagePath;
   return `${imagePath.slice(0, -6)}sm.png`;
 }
 
+imageElements.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    img.src = setImagePath(e.currentTarget.src);
+
+    lightboxElement.classList.add("active");
+  });
+});
+
 lightboxElement.addEventListener("click", (e) => {
   if (e.target.id !== e.currentTarget.id && screen.width > 540) return;
   lightboxElement.classList.remove("active");
 });
+*/
